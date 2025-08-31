81f5a139fdfb7ffffacaed5d277755a4b893f148
ROHIT PARSODE <rohitaparsode.cse23d@kdkce.edu.in>
2025-08-31 17:37:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -22,6 +22,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.openfoodfacts.org',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
