78dc1b333325d4757faf56a080a3b027b2592dfe
ROHIT PARSODE <rohitaparsode.cse23d@kdkce.edu.in>
2025-09-10 20:05:40 +0000

    Try fixing this error: `Runtime Error: Error: Invalid src prop (https://

--- next.config.ts
+++ next.config.ts
@@ -46,6 +46,12 @@ const nextConfig: NextConfig = {
         port: '',
         pathname: '/**',
       },
+      {
+        protocol: 'https',
+        hostname: 'images.albertsons-media.com',
+        port: '',
+        pathname: '/**',
+      },
     ],
   },
 };
